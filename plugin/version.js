226174a2cd0f7b2722efd862f5f25d84368487ae
duhongwei <757318006@qq.com>
2019-10-24 15:15:02 +0800

    fix version plugins for dev

--- plugin/version.js
+++ plugin/version.js
@@ -1,20 +1,26 @@
 
 const debug = require('debug')('hotpack/version')
-const { isHtml, isText,isLess } = require('../lib/util')
+const { isHtml, isText, isLess } = require('../lib/util')
 module.exports = function () {
-  return function (files, { version, logger }) {
-    logger.log('run plugin version')
+  return function (files, spack) {
+    spack.logger.log('run plugin version')
 
     for (let file in files) {
       if (isHtml(file)) {
         continue
       }
       if (isText(file) && files[file].contents.trim() === '') {
-        logger.fatal(`${file} is empty`)
-        process.exit(1)
+        spack.logger.fatal(`${file} is empty`)
+        if (spack.env == 'production') {
+          process.exit(1)
+        }
+        else {
+          delete files[file]
+          continue
+        }
       }
-      
-      if (version.update(file, files[file].contents)) {
+
+      if (spack.version.update(file, files[file].contents)) {
         debug(`${file} updated`)
       }
       else {
